e4f74e1c1a505522c250180a26d60417145100bf
Piachsecki <k.r.piasecki2@gmail.com>
2023-03-12 21:09:04 +0100

    4

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ public class Main {
 
     public static final String EX_2 = "./src/exported/ex2/";
     public static final String EX_3 = "./src/exported/ex3/";
+    public static final String EX_4 = "./src/exported/ex4/";
     public static final String PURCHASES_OF = "purchases-of-";
 
     public static void main(String[] args) {
@@ -26,6 +28,68 @@ public class Main {
         Map<String, List<Purchase>> mapByCompany = purchases.stream()
                 .collect(Collectors.groupingBy(purchase -> purchase.getCar().getCompany()));
 
+        ex3(mapByCompany);
+
+
+        ex2(mapByCompany);
+
+        // id, date, count
+        TreeMap<LocalDate, Long> mapByDate = purchases.stream()
+                .collect(Collectors.groupingBy(
+                        Purchase::getDate,
+                        TreeMap::new,
+                        Collectors.counting()
+                ));
+        AtomicInteger counter = new AtomicInteger(1);
+        List<String> databyDate = mapByDate.entrySet().stream()
+                .map(e -> String.format("%s,%s,%s", counter.getAndIncrement(), e.getKey(), e.getValue()))
+                .toList();
+
+        generateEx4report(databyDate,"byDate");
+
+        List<String> list = mapByDate.entrySet().stream()
+                .map(e -> new Pair<>(e.getValue(), e.getKey()))
+                .sorted(Comparator.comparing((Pair<Long, LocalDate> p) -> p.u()).reversed())
+                .map(p -> String.format("%s,%s,%s", counter.getAndIncrement(), p.t(), p.u()))
+                .toList();
+
+    }
+
+    private static void generateEx4report(List<String> databyDate, String suffix) {
+        Path path = Paths.get(EX_4 + suffix +".csv");
+        FileService.saveToFile(path, databyDate, "id,date,count");
+
+    }
+
+    private static void ex2(Map<String, List<Purchase>> mapByCompany) {
+        for (Map.Entry<String, List<Purchase>> entry : mapByCompany.entrySet()) {
+            Path path1 = Paths.get(EX_2 + PURCHASES_OF + entry.getKey() + ".csv");
+            List<String> data = entry.getValue().stream()
+                    .map(PurchaseMappingService::toCsvRow)
+                    .toList();
+            FileService.saveToFile(path1, data);
+        }
+
+        try {
+              TreeMap<Path, ? extends Number> mapSizeByCompany = Files.list(Paths.get(EX_2))
+                      .collect(Collectors.toMap(
+                              Path::getFileName,
+                              Main::getFileSize,
+                              (a, b) -> a,
+                              () -> new TreeMap<>(Comparator.reverseOrder())));
+
+              for (Path fileName : mapSizeByCompany.keySet()) {
+                  String substringOfNameFile = fileName.toString().substring(PURCHASES_OF.length(), fileName.toString().indexOf('.'));
+                  System.out.printf("%s:%s%n",
+                          substringOfNameFile,
+                          mapSizeByCompany.get(fileName));
+              }
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+    }
+
+    private static void ex3(Map<String, List<Purchase>> mapByCompany) {
         Map<String, Map<String, List<Purchase>>> mapByCompanyAndModel = mapByCompany.entrySet().stream()
                 .collect(Collectors.toMap(
                                 entry -> entry.getKey(),
@@ -59,36 +123,6 @@ public class Main {
                 .toList();
 
         generateEx3report(reportMapData);
-
-
-        for (Map.Entry<String, List<Purchase>> entry : mapByCompany.entrySet()) {
-            Path path1 = Paths.get(EX_2 + PURCHASES_OF + entry.getKey() + ".csv");
-            List<String> data = entry.getValue().stream()
-                    .map(PurchaseMappingService::toCsvRow)
-                    .toList();
-            FileService.saveToFile(path1, data);
-        }
-
-      try {
-            TreeMap<Path, ? extends Number> mapSizeByCompany = Files.list(Paths.get(EX_2))
-                    .collect(Collectors.toMap(
-                            Path::getFileName,
-                            Main::getFileSize,
-                            (a, b) -> a,
-                            () -> new TreeMap<>(Comparator.reverseOrder())));
-
-            for (Path fileName : mapSizeByCompany.keySet()) {
-                String substringOfNameFile = fileName.toString().substring(PURCHASES_OF.length(), fileName.toString().indexOf('.'));
-                System.out.printf("%s:%s%n",
-                        substringOfNameFile,
-                        mapSizeByCompany.get(fileName));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-//
-//        purchases.forEach(System.out::println);
     }
 
     private static void generateEx3report(List<String> reportMapData) {
